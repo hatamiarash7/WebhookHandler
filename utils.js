14dd13a711630bbb3c08f64e7df9e4b26f07f041
Arash Hatami <hatamiarash7@gmail.com>
2020-09-20 15:54:45 +0430

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,16 @@
+const getHeader = function (provider) {
+  switch (provider) {
+    case "github":
+      return "X-Hub-Signature";
+    case "gitlab":
+      return "X-Gitlab-Token";
+    case "gogs":
+      return "X-Gogs-Signature";
+    case "gitea":
+      return "HTTP_X_GITEA_SIGNATURE";
+    default:
+      return "error";
+  }
+};
+
+module.exports = { getHeader };
