ec88da08d445a6aaa1784a503ba6dca93d456883
Arash Hatami <hatamiarash7@gmail.com>
2020-09-20 15:54:43 +0430

    Create verification.js

---
+++ verification.js
@@ -0,0 +1,19 @@
+const crypto = require("crypto");
+
+function sign(data, secret) {
+  const hmac = crypto.createHmac("sha1", secret);
+  const ourSignature = `sha1=${hmac.update(data).digest("hex")}`;
+
+  return ourSignature;
+}
+
+exports.verify = function verify(signature, data, secret) {
+  const sig = Buffer.from(signature, "utf8");
+  const signed = Buffer.from(sign(data, secret), "utf8");
+
+  if (sig.length !== signed.length) {
+    return false;
+  }
+
+  return crypto.timingSafeEqual(sig, signed);
+};
