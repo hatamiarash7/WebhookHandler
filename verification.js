f58e358a676fa1049e221cb5003a062a0451e5d9
Arash Hatami <info@arash-hatami.ir>
2021-10-13 11:14:13 +0330

    Use simple format

--- verification.js
+++ verification.js
@@ -3,7 +3,6 @@ const crypto = require("crypto");
 function sign(data, secret) {
   const hmac = crypto.createHmac("sha1", secret);
   const ourSignature = `sha1=${hmac.update(data).digest("hex")}`;
-
   return ourSignature;
 }
 
@@ -11,9 +10,9 @@ exports.verify = function verify(signature, data, secret) {
   const sig = Buffer.from(signature, "utf8");
   const signed = Buffer.from(sign(data, secret), "utf8");
 
-  if (sig.length !== signed.length) {
+  if (sig.length !== signed.length || !crypto.timingSafeEqual(sig, signed)) {
     return false;
+  } else {
+    return true;
   }
-
-  return crypto.timingSafeEqual(sig, signed);
 };
